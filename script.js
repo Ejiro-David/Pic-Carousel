93b44156c4eb309823f4f35b2556215917183814
EJ-David <daviderhabor.de@gmail.com>
2022-11-07 13:52:33 +0100

    style: implemented basic css

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('hello')
+
+const imgSrc = ['./images/hacksultan.jpg', './images/myblogsc.png', './images/scofield.jpeg', './images/seths.jpeg', './images/snailmeme.jpeg', './images/yinyang.webp']
+const imgAlt = ['hack sultan on the windows xp wallpapper', 'screenshot from a recent blog of mine', 'Micheal Scofield from Prison Break', "screenshot from Seth Godin's Blog", "meme", "Yin Yang"]
+
+
+const imgData = {
+    
+}
+
+
