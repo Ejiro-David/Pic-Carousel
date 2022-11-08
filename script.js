b75a8ffc10b26844fcc0d0b0e0d55c7e9449c626
EJ-David <daviderhabor.de@gmail.com>
2022-11-08 17:10:16 +0100

    scroll on click funtionality added

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-console.log('hello')
+console.log("hello");
+var x = 0;
 
-const imgSrc = ['./images/hacksultan.jpg', './images/myblogsc.png', './images/scofield.jpeg', './images/seths.jpeg', './images/snailmeme.jpeg', './images/yinyang.webp']
-const imgAlt = ['hack sultan on the windows xp wallpapper', 'screenshot from a recent blog of mine', 'Micheal Scofield from Prison Break', "screenshot from Seth Godin's Blog", "meme", "Yin Yang"]
+const imgSrc = [
+    "./images/hacksultan.jpg",
+    "./images/myblogsc.png",
+    "./images/scofield.jpeg",
+    "./images/seths.jpeg",
+    "./images/snailmeme.jpeg",
+    "./images/yinyang.webp",
+];
+const imgAlt = [
+    "hack sultan on the windows xp wallpapper",
+    "screenshot from a recent blog of mine",
+    "Micheal Scofield from Prison Break",
+    "screenshot from Seth Godin's Blog",
+    "meme",
+    "Yin Yang",
+];
 
+const imgData = {};
 
-const imgData = {
-    
-}
-
-function pickSrc(x){
-    if(x === 5){
-        return 0
-    }else{
-        return x + 1
+function pickSrc(y) {
+    if (y === 5) {
+        return 0;
+    } else {
+        return y + 1;
     }
 }
-
-function slide(){
-    console.log('pic should slide')
+var x = 1;
+function slide() {
     let myImg = document.getElementById("img");
+    console.log("click acknowledged");
+
+    myImg.setAttribute('src', imgSrc[ x >= 5 ? x = 0 : x++], console.log('src set'));
+    
 }
-window.addEventListener('click', slide)
+window.addEventListener("click", slide);
