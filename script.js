73c3f2dc22a6848e3d80935073cd05daa62dfaf2
EJ-David <daviderhabor.de@gmail.com>
2022-11-10 15:37:23 +0100

    bug: fixed click miss behaviour

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 console.log("hello");
 var x = 0;
+const LeftClick = document.getElementById('lr');
+const RightClick = document.getElementById("rr");
+
+
+
+
+function ack(){
+    console.log('click logged')
+}
+
+
+
+
+
 
 const imgSrc = [
-    "./images/hacksultan.jpg",
-    "./images/myblogsc.png",
-    "./images/seths.jpeg",
-    "./images/snailmeme.jpeg",
-    "./images/yinyang.webp",
-    "./images/putinspittin.jpeg"
+  "./images/hacksultan.jpg",
+  "./images/myblogsc.png",
+  "./images/seths.jpeg",
+  "./images/snailmeme.jpeg",
+  "./images/yinyang.webp",
+  "./images/putinspittin.jpeg",
 ];
 const imgAlt = [
-    "hack sultan on the windows xp wallpapper",
-    "screenshot from a recent blog of mine",
-    "screenshot from Seth Godin's Blog",
-    "meme",
-    "Yin Yang",
-    "putin speaks some hard truths"
+  "hack sultan on the windows xp wallpapper",
+  "screenshot from a recent blog of mine",
+  "screenshot from Seth Godin's Blog",
+  "meme",
+  "Yin Yang",
+  "putin speaks some hard truths",
 ];
 
 const imgData = {};
 
-function pickSrc(y) {
-    if (y === 5) {
-        return 0;
-    } else {
-        return y + 1;
-    }
-}
+        
 var x = 1;
-function slide() {
-    let myImg = document.getElementById("img");
-    console.log("click acknowledged");
 
-    myImg.setAttribute('src', imgSrc[ x >= 7 ? x = 0 : x++], console.log('src set'));
-    
+const setSrc = (ind) => {
+    myImg = document.getElementById("img");
+    myImg.setAttribute('src', imgSrc[ind])
+    myImg.setAttribute('alt', imgAlt[ind])
 }
-window.addEventListener("click", slide);
+function swipe(event){
+    console.log(event.target.id)
+    if(event.target.id === 'lr'){
+        if(x > 0){
+            x--
+        }else{
+            x = imgAlt.length - 1;
+        }
+        setSrc(x)
+    }else{
+        if(x < 6){
+            x++
+        }else{
+            x = 0
+        }
+        setSrc(x)
+    }
+
+
+}
+
